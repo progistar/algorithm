b34da963eef314acbb970ca8e45a51d5729b31bf
gistar <progistar@naver.com>
2017-12-24 17:29:53 +0900

    LIS: BinarySearch

--- ProblemSolvingBooks_SH/src/lis/BinarySearch.java
+++ ProblemSolvingBooks_SH/src/lis/BinarySearch.java
@@ -1,5 +1,7 @@
 package lis;
 
+import java.util.Arrays;
+
 public class BinarySearch {
 
 	public static void main(String[] args) {
@@ -11,27 +13,37 @@ public class BinarySearch {
 	public static int findLIS(int[] set) {
 		int max = 0;
 		int maxLen = set.length;
+		int[] binaryIndex = new int[set.length];
+		Arrays.fill(binaryIndex, Integer.MAX_VALUE);
 		
-		for(int i=0; i<maxLen; i++) {
-			int lis = findLIS(set, i);
-			max = max > lis ? max : lis;
-		}
+		// init first step
+		binaryIndex[0] = set[0];
 		
-		return max;
+		for(int i=1; i<maxLen; i++) findLIS(binaryIndex, set[i]);
+		for(max=0; max<maxLen; max++) if(binaryIndex[max] == Integer.MAX_VALUE) break;
+		
+		return (max);
 	}
 	
-	public static int findLIS(int[] set, int startIndex) {
+	public static void findLIS(int[] binaryIndex, int pivot) {
 		int max = 0;
-		int maxLen = set.length;
+		// binary search on 'binary index'
 		
-		//
-		for(int i=startIndex+1; i< maxLen; i++) {
-			int lis = 0;
-			if(set[startIndex] < set[i]) lis = findLIS(set, i);
-			max = max > lis ? max : lis;  
+		int bottom = 0;
+		int top = binaryIndex.length-1;
+		
+		while(true) {
+			int index = (bottom+top+1)/2;
+			if(binaryIndex[index-1] < pivot && binaryIndex[index] > pivot) {
+				binaryIndex[index] = pivot; break;
+			}else if(index == 1) {
+				binaryIndex[0] = pivot; break;
+			}
+			
+			if(binaryIndex[index] == pivot) break;
+			if(binaryIndex[index] < pivot) bottom = index;
+			else top = index;
+			
 		}
-		
-		// It is length 1 lis itself.
-		return (max+1);
 	}
 }
