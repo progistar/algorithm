fae6d241ef696a6649c1a3fad38caacd25bedf01
gistar <progistar@naver.com>
2018-01-21 21:47:50 +0900

    p2133: solved!

--- Backjoon_SH/src/p2133/Main.java
+++ Backjoon_SH/src/p2133/Main.java
@@ -1,5 +1,6 @@
 package p2133;
 
+import java.util.Hashtable;
 import java.util.Scanner;
 
 public class Main {
@@ -10,34 +11,16 @@ public class Main {
 		int n = scan.nextInt();
 		scan.close();
 		
-		System.out.println(draw(n, new int[n+1]));
-		System.out.println(brute(new boolean[n][3], 0, 0));
+		System.out.println(brute(new boolean[n][3], 0, 0, new int[n][3][n][3]));
 	}
 	
-	public static int draw(int blocks, int[] memorization) {
-		if(blocks % 2 == 1) return 0;
-		if(blocks == 2) return 3;
-		if(blocks < 2) return 0;
-		if(memorization[blocks] != 0) return 2;
+	public static int brute(boolean[][] blocks, int row, int col, int[][][][] memorization) {
 		
-		int baseCase = 2;
-		
-		int left = 2;
-		int right = blocks - left;
-		while(right > 0) {
-			baseCase += draw(left, memorization) * draw(right, memorization);
-			right -= 2;
-			left += 2;
-		}
-		
-		memorization[blocks] = baseCase;
-		return baseCase;
-	}
-	
-	public static int brute(boolean[][] blocks, int row, int col) {
 		int sizeOfRow = blocks.length;
 		int sizeOfCol = 3;
 		int total = 0;
+		
+		if(sizeOfRow % 2 == 1) return 0;
 		
 		if(row == sizeOfRow-1 && col == 2) {
 			if(blocks[row][col]) return 1;
@@ -48,7 +31,7 @@ public class Main {
 		int nextRowStep = nextColStep == 0 ? row+1 : row;
 		
 		if(blocks[row][col]) {
-			total = brute(blocks, nextRowStep, nextColStep);
+			total = brute(blocks, nextRowStep, nextColStep, memorization);
 			return total;
 		}
 
@@ -56,17 +39,26 @@ public class Main {
 		
 		// vertical
 		if(row+1 < sizeOfRow && (blocks[row+1][col] == false)) {
-			boolean[][] newBlocks = copyBlocks(blocks);
-			newBlocks[row+1][col] = true;
-			total += brute(newBlocks, nextRowStep, nextColStep);
+			if(memorization[row][col][row+1][col] != 0) total += memorization[row][col][row+1][col];
+			else {
+				boolean[][] newBlocks = copyBlocks(blocks);
+				newBlocks[row+1][col] = true;
+				memorization[row][col][row+1][col] = brute(newBlocks, nextRowStep, nextColStep, memorization);
+				total += memorization[row][col][row+1][col];
+			}
 		}
 		
 		// horizontal
 		if(col+1 < sizeOfCol && (blocks[row][col+1] == false)) {
-			boolean[][] newBlocks = copyBlocks(blocks);
-			newBlocks[row][col+1] = true;
-			total += brute(newBlocks, nextRowStep, nextColStep);
+			if(memorization[row][col][row][col+1] != 0) total += memorization[row][col][row][col+1];
+			else {
+				boolean[][] newBlocks = copyBlocks(blocks);
+				newBlocks[row][col+1] = true;
+				memorization[row][col][row][col+1] = brute(newBlocks, nextRowStep, nextColStep, memorization);
+				total += memorization[row][col][row][col+1];
+			}
 		}
+		
 		
 		return total;
 	}
